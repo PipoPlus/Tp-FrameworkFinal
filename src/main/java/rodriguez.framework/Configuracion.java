6fb956b4ef4aa8c4517ae4f4ba7a850190931e2f
PipoPlus <pipoplusnft@gmail.com>
2024-06-20 15:25:36 -0300

    Punto 4 Terminado

--- src/main/java/rodriguez.framework/Configuracion.java
+++ src/main/java/rodriguez.framework/Configuracion.java
@@ -2,33 +2,42 @@ package rodriguez.framework;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 public class Configuracion {
 
-    public List<Accion> configToActionsList(String path){
+    public List<Accion> configToActionsList(String path) {
         List<Accion> acciones = new ArrayList<>();
 
-        Properties properties = new Properties();
-        try (FileInputStream input = new FileInputStream(path)){
-            properties.load(input);
+        try (InputStream input = new FileInputStream(path)) {
+            if (path.endsWith(".properties")) {
+                // Manejar archivo .properties
+                Properties properties = new Properties();
+                properties.load(input);
 
-            for (String key : properties.stringPropertyNames()){
-                String nombreDeClase = properties.getProperty(key);
-                try{
-                    Class<?> clazz = Class.forName(nombreDeClase);
-
-                    Accion accion = (Accion) clazz.getConstructor().newInstance();
-
-                    acciones.add(accion);
-                }catch (Exception e) {
-                    System.out.println("Error al cargar la clase: " + nombreDeClase);
-                    e.printStackTrace();
+                for (String key : properties.stringPropertyNames()) {
+                    acciones.add(crearAccion(properties.getProperty(key)));
                 }
+
+            } else if (path.endsWith(".json")) {
+                // Manejar archivo .json
+                JSONObject jsonObject = new JSONObject(new JSONTokener(input));
+                JSONArray accionesArray = jsonObject.getJSONArray("acciones");
+
+                for (int i = 0; i < accionesArray.length(); i++) {
+                    acciones.add(crearAccion(accionesArray.getString(i)));
+                }
+
+            } else {
+                System.out.println("Formato de archivo no soportado.");
             }
-        }catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("Error al leer el archivo de configuración.");
             e.printStackTrace();
         }
@@ -36,7 +45,14 @@ public class Configuracion {
         return acciones;
     }
 
-
-
-
+    private Accion crearAccion(String nombreDeClase) {
+        try {
+            Class<?> clazz = Class.forName(nombreDeClase);
+            return (Accion) clazz.getConstructor().newInstance();
+        } catch (Exception e) {
+            System.out.println("Error al cargar la clase: " + nombreDeClase);
+            e.printStackTrace();
+            return null; // O lanzar una excepción personalizada
+        }
+    }
 }
--- src/main/java/rodriguez.main/Main.java
+++ src/main/java/rodriguez.main/Main.java
@@ -1,16 +1,15 @@
 package rodriguez.main;
 
-
 import rodriguez.framework.Configuracion;
 import rodriguez.framework.Start;
 
 
+
 public class Main {
-    public static void main(String[]args){
+    public static void main(String[]args) {
         Configuracion config = new Configuracion();
 
-        Start m = new Start(config.configToActionsList("C:\\Users\\elrod\\OneDrive\\Escritorio\\Cosas\\Uni\\OO2\\TRABAJO FINAL FRAMEWORK" +
-                "\\Tp-Framework\\src\\main\\resources\\config.properties"));
+        Start m = new Start(config.configToActionsList("C:\\Users\\elrod\\OneDrive\\Escritorio\\Cosas\\Uni\\OO2\\TRABAJO FINAL FRAMEWORK\\Tp-Framework\\src\\main\\resources\\config.properties"));
         m.init();
     }
 }
